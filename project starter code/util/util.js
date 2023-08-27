97c20d9fff3c9b8ae67d0be140029680db2f231d
vunguyengitacc <nguyenhoanganhvu.it@gmail.com>
2023-08-27 20:10:05 +0700

    fix error cannot fetch image

--- project starter code/util/util.js
+++ project starter code/util/util.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import Jimp from "jimp";
+import axios from 'axios';
 
 
 // filterImageFromURL
@@ -12,7 +13,10 @@ import Jimp from "jimp";
  export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      let photoBuf = await axios.get(inputURL, {
+        responseType: "arraybuffer"
+      })
+      const photo = await Jimp.read(Buffer.from(photoBuf?.data, "binary"));
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
